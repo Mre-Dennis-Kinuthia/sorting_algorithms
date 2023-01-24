f23170185a7ee3ab98dc6f1a36c60601f6014c68
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-24 15:11:17 +0000

    init

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - entry point
+ * 
+ * return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    int i;
+    int temp;
+
+    for(; i < size; i++)
+    {
+        if(i > array[i])
+        {
+            temp = array[i]
+            
+        }
+    }
+}
