a47398e6e3c011f5fa1523e23b41b46b992b8a41
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-24 17:12:43 +0000

    init

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,28 @@
 #include "sort.h"
 
-/**
- * insertion_sort_list - sorts a doubly linked list of integers using
- *                       the insertion sort algorithm
- *
- * @list: doubly linked list of integers to be sorted
- * Return: nothing
- */
-
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current = *list, *temp;
+listint_t *current = *list;
+listint_t *previous;
 
-    if (current == NULL || current->next == NULL)
-        return;
+while (current)
+{
+previous = current;
+current = current->next;
 
-    while (current)
-    {
-        temp = current;
-
-        while (temp->prev && temp->prev->n > temp->n)
-        {
-            temp->prev->next = temp->next;
-
-            if (temp->next)
-                temp->next->prev = temp->prev;
-
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
-
-            if (!temp->prev)
-                *list = temp;
-            else
-                temp->prev->next = temp;
-
-            print_list(*list);
-        }
-        current = current->next;
-    }
+while (previous->prev && previous->n < previous->prev->n)
+{
+previous->prev->next = previous->next;
+if (previous->next)
+previous->next->prev = previous->prev;
+previous->next = previous->prev;
+previous->prev = previous->prev->prev;
+previous->next->prev = previous;
+if (previous->prev)
+previous->prev->next = previous;
+else
+*list = previous;
+print_list(*list);
+}
+}
 }
