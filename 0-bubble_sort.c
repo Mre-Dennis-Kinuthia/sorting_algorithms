3180e89dfb92d20fe9834c397de375b91814fac8
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-24 16:20:17 +0000

    init

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,12 +7,14 @@
  * ascending order using the Bubble sort algorithm
  * @array: array of integers
  * @size: size of array
+ *
+ * Return: nothing
  **/
 
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
     size_t i, j ;
+    int temp = 0;
     for(i = 0; i < size; i++)
     {
         for (j = 0; j < size - 1; j++)
