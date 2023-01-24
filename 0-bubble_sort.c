c0e648d00ea39b75721e855792910fd66358718f
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-24 16:25:59 +0000

    init

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,19 +13,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j ;
-    int temp = 0;
-    for(i = 0; i < size; i++)
+    size_t i, j;
+    int temp, swapped;
+
+    for (i = 0; i < size; i++)
     {
+        swapped = 0;
         for (j = 0; j < size - 1; j++)
         {
-            if(array[j] > array[j + 1])
+            if (array[j] > array[j + 1])
             {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                swapped = 1;
                 print_array(array, size);
             }
         }
+        if (!swapped)
+            break;
     }
 }
