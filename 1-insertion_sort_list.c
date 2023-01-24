d7dfb62e81be1468b3725cad454fa6bace66b9da
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-24 17:05:35 +0000

    init

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,27 +2,27 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current = *list;
-    listint_t *previous;
+listint_t *current = *list;
+listint_t *previous;
 
-    while (current)
-    {
-        previous = current;
-        current = current->next;
+while (current)
+{
+previous = current;
+current = current->next;
 
-        while (previous->prev && previous->n < previous->prev->n)
-        {
-            previous->prev->next = previous->next;
-            if (previous->next)
-                previous->next->prev = previous->prev;
-            previous->next = previous->prev;
-            previous->prev = previous->prev->prev;
-            previous->next->prev = previous;
-            if (previous->prev)
-                previous->prev->next = previous;
-            else
-                *list = previous;
-            print_list(*list);
-        }
-    }
+while (previous->prev && previous->n < previous->prev->n)
+{
+previous->prev->next = previous->next;
+if (previous->next)
+previous->next->prev = previous->prev;
+previous->next = previous->prev;
+previous->prev = previous->prev->prev;
+previous->next->prev = previous;
+if (previous->prev)
+previous->prev->next = previous;
+else
+*list = previous;
+print_list(*list);
+}
+}
 }
