2444d7ba964c283c4f009a383e7c49b67ff11445
Dennis Kinuthia <DennisKinuthia1105@gmail.com>
2023-01-24 16:08:51 +0000

    init

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,10 +3,11 @@
 #include "sort.h"
 
 /**
- * bubble_sort - entry point
- * 
- * return: void
- */
+ * bubble_sort - a function that sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: array of integers
+ * @size: size of array
+ **/
 
 void bubble_sort(int *array, size_t size)
 {
@@ -14,13 +15,13 @@ void bubble_sort(int *array, size_t size)
     size_t i, j ;
     for(i = 0; i < size; i++)
     {
-        for (j = 0; j < size - 1; i++)
+        for (j = 0; j < size - 1; j++)
         {
-            if(array[j] > array[j+1])
+            if(array[j] > array[j + 1])
             {
                 temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
                 print_array(array, size);
             }
         }
--- print_array.c
+++ print_array.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+
 void print_array(const int *array, size_t size)
 {
     size_t i;
